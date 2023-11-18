376f808d1c4d13350ab081087bfe9027b2759e47
Nitish313 <student.nitishanand@gmail.com>
2023-11-18 17:29:07 +0530

    Create reusable articles component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,10 +17,6 @@ const Home = () => {
       <p className='mx-auto leading-relaxed text-base mb-4'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, est doloremque. Porro adipisci eveniet aspernatur quae voluptate unde iusto obcaecati impedit quis perspiciatis qui facere fuga nam placeat, officia a ipsam. Nihil veritatis repellat alias et fugiat dolores architecto? Itaque magni ipsam aliquam harum velit dolore quas, minima, eligendi deserunt eius pariatur facere illo. Ipsam, sint exercitationem aut illum sed velit beatae, suscipit quia earum, veritatis dolorum delectus dolores deserunt dolor sapiente tempore. Minima sed sunt, repudiandae odio laborum eveniet ab omnis quae soluta blanditiis velit fugiat adipisci nam quaerat quos nihil delectus accusamus voluptatibus veniam fugit. Exercitationem, minima architecto.
       </p>
-
-      <p><a href='/about'>About</a></p>
-      <p><a href='/articles'>Articles</a></p>
-      <p><a href='/article'>Article</a></p>
     </div>
   )
 }
