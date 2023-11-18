76f8cb7c61a0cf3975c1f90c1bb6a6d58e086aaf
Nitish313 <student.nitishanand@gmail.com>
2023-11-18 15:29:25 +0530

    Add About, Article, ArticleList and Home pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import Home from './pages/Home';
+import About from './pages/About';
+import Article from './pages/Article';
+import ArticleList from './pages/ArticleList';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -7,6 +11,9 @@ function App() {
       <div className='max-w-screen-md mx-auto pt-20'>
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/articles' element={<ArticleList />}/>
+          <Route path='/article' element={<Article />}/>
+          <Route path='/about' element={<About />}/>
         </Routes>
       </div>
     </Router>
---
+++ src/pages/ArticleList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArticleList = () => {
+  return(
+    <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-600'>
+      Articles
+    </h1>
+  )
+}
+
+export default ArticleList
